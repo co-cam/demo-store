06cea304584895c6749b1598c2787b4333ef9b74
di3upham <di3upham@gmail.com>
2025-07-11 18:56:00 +0700

    add react strict-mode

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false, // true in dev mode
 };
 
 export default nextConfig;
