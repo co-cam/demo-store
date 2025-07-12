7cb9e6b0f6455245d69a6547b6ef70ad54b951d2
cpfriend1721994 <hedspi1721994@gmail.com>
2025-07-12 09:29:15 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  reactStrictMode: false, // true in dev mode
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+initOpenNextCloudflareForDev();
