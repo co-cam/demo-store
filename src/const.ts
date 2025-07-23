ae5c225f93e5ac6893fa7a30959162896034a896
di3upham <di3upham@gmail.com>
2025-07-23 15:48:59 +0700

    new merchant config

--- src/const.ts
+++ src/const.ts
@@ -2,16 +2,11 @@
 
 // TODO replace with actual merchant
 export const Payment_js_src = 'https://checkout.sandbox.whatee.store/sdk.js'
-export const Payment_merchant_id = 'd9cafc93-eacb-42ec-a3ce-2e651df48947';
+export const Payment_merchant_id = '4c82c4d9-f8f2-4740-8829-8b8f6972ea78';
 export const Payment_api_url = 'https://onecheckout.sandbox.whatee.io/api/v1.0/orders';
-export const Payment_api_key = '4QYyU2g9BYbbsIAA7CRyDdAWvA4TwNrbha7s0wxRXbG4c5tEN27OtaWX9YJhkXlVvo0uy2p8wqV6qlWGm9Wjn28Ef2zHw4z0OmoIxtNgOCe69kflffiH9FKrJ8uHJk3f';
+export const Payment_api_key = 'riTsflJlFlllW0JHJN2l7jd2P11Imw5M3ONvanrhwWsII4T8TJPnH3pXiOkgZ8Yq4FrpKTksX7PUd63jevgIJ0Oig82qCtqRnvMFdkwHnUhCD45On8vE5FNYrkWRvN9s';
 
 // export const Payment_js_src = 'http://localhost:3000/sdk.js'
-// export const Payment_merchant_id = '1e77a9c2-8c7b-474f-a8d9-ddabdfb96ae5';
+// export const Payment_merchant_id = '8a6d4b93-b81e-42d9-b92d-eb130c0d5f07';
 // export const Payment_api_url = 'http://localhost:9000/api/v1.0/orders';
-// export const Payment_api_key = 'rEosSSBgTI8VOrhsiHdUNfkSMviCJaSoYAI8sjtb6PnsGV1un96ithUrw2hhpJS8W1ukiOGAzGixfcTWO26N8uHYWdjv5t8sC3sv3jm5TgWV84Z9PPlNzGS0Xjdxr0Qt';
-
-// export const Payment_js_src = 'http://localhost:3000/sdk.js'
-// export const Payment_merchant_id = 'e22fc74e-74f8-41d7-aec4-055a17480b2e';
-// export const Payment_api_url = 'http://localhost:9000/api/v1.0/orders';
-// export const Payment_api_key = 'UgheInSylnUwHLrSgaNLEv7IL9EpHpgoY8MvloegcptoEJFA9Gv7abzu6nfTnudvr5JGXhOvlfPeV1ESLOZKgHVOXn27h9JNZ7lIyjLx0O8BttXI8Flq2oSv7YQvmE5L';
+// export const Payment_api_key = '2N7c6aRYwyRWBu5uxAcM0SDqKmDh3Tb4BvjNJM4wY3O5Yn5czLqEnlOObY4QMgUZmV5H3LI75QIl4YmdKIO3PMWsvD9IOCX2UrxY2rGKEkRyWUJpWVzlQMz5BrZIiX7N';
