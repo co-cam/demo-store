bc95f3f8cceb83e95f0b9dd402222b43188ae5f7
Trung <ndtrung122@gmail.com>
2025-08-01 18:32:32 +0700

    Update api key test

--- src/const.ts
+++ src/const.ts
@@ -7,9 +7,9 @@
 // export const Payment_api_key = 'riTsflJlFlllW0JHJN2l7jd2P11Imw5M3ONvanrhwWsII4T8TJPnH3pXiOkgZ8Yq4FrpKTksX7PUd63jevgIJ0Oig82qCtqRnvMFdkwHnUhCD45On8vE5FNYrkWRvN9s';
 
 export const Payment_js_src = 'https://checkout.sandbox.whatee.store/sdk.js'
-export const Payment_merchant_id = 'ea5168a1-7f90-4486-a2c2-e12cf43dac63';
+export const Payment_merchant_id = '4c545235-b950-4dc2-8d76-c2b8d806ce5a';
 export const Payment_api_url = 'https://onecheckout.sandbox.whatee.io/api/v1.0/orders';
-export const Payment_api_key = 'V3bOo0vkBmgGV77Y8mRRoTjjDc4GhdvDcxEXbuBMepvsBdzmkaTq6AniG4JrCd68XwwPR73wi4k0byrHYlc9JAoertsyBekvwqPGaz1zYFaiNyvIkDsXqReUp1QECmg4';
+export const Payment_api_key = 'DZkRXORebFa7uc5S6ehpQI4mCaI9X6788mkpEibGYAf41wNd8OmdP0PjLPvoCX1INxuGvfK24JZLkMWiQLptg9PBU8KzrUwcuLUgdhAmEuGYL9bfcd3xEVzROgCc5IGT';
 
 // export const Payment_js_src = 'http://localhost:3000/sdk.js'
 // export const Payment_merchant_id = 'eb0fb0db-1c11-4a43-9fa7-bed6b9d0010e';
