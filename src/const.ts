7d5cd30d11611774ddae5af782e718137ce5492c
di3upham <di3upham@gmail.com>
2026-03-03 14:40:53 +0700

    update const

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,16 @@
 // canbe read from environment variables or a config file
 
 // TODO replace with actual merchant
+// export const Payment_js_src = 'https://game.sandbox.whatee.store/checkout-page/sdk.js'
+// export const Payment_merchant_id = '4c545235-b950-4dc2-8d76-c2b8d806ce5a';
+// export const Payment_api_url = 'https://onecheckout.sandbox.whatee.io/api/v1.0/orders';
+// export const Payment_api_key = 'DZkRXORebFa7uc5S6ehpQI4mCaI9X6788mkpEibGYAf41wNd8OmdP0PjLPvoCX1INxuGvfK24JZLkMWiQLptg9PBU8KzrUwcuLUgdhAmEuGYL9bfcd3xEVzROgCc5IGT';
+
+
 export const Payment_js_src = 'https://game.sandbox.whatee.store/checkout-page/sdk.js'
-export const Payment_merchant_id = '4c545235-b950-4dc2-8d76-c2b8d806ce5a';
+export const Payment_merchant_id = '4dd35c09-20cf-4f0e-b565-6e0fc2623e0e';
 export const Payment_api_url = 'https://onecheckout.sandbox.whatee.io/api/v1.0/orders';
-export const Payment_api_key = 'DZkRXORebFa7uc5S6ehpQI4mCaI9X6788mkpEibGYAf41wNd8OmdP0PjLPvoCX1INxuGvfK24JZLkMWiQLptg9PBU8KzrUwcuLUgdhAmEuGYL9bfcd3xEVzROgCc5IGT';
+export const Payment_api_key = 'e9zGiZq54HqkCVaYTP13dAxjnEbjYFBucYth9YI0Q0ltJQ2YU2gWN60vnxvJ9WcAsq6EV6L3b4qvMwz3QgxMG3H4INBHFGdjaSE6JrJ40hJc7HV0rdgm7A5GDTGWRvYH';
 
 // export const Payment_js_src = 'http://localhost:3000/checkout-page/sdk.js'
 // export const Payment_merchant_id = '264f4a18-ba70-4038-ae18-39f5bd94f014';
